b5fc7d4cd635a2eb94d48029f635f87a0a53398e
Al-xandr1 <al-xandr1@yandex.ru>
2015-03-19 23:09:37 +0400

    added Hotspot & RootFinder projects

---
+++ Hotspot/main.cpp
@@ -0,0 +1,286 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <cstdlib>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+struct Waypoint{
+    double X,Y,Tb,Te;
+    char* traceName;
+};
+
+struct Cluster {
+     double Xmin, Xmax, Ymin, Ymax;
+     double sumTime;
+     vector<Waypoint> waypoint;
+};
+
+struct Location {
+    double Xmin, Xmax, Ymin, Ymax;
+    double sumTime;
+    unsigned long int wPoints;
+};
+
+char* buildFullName(char* buffer, char* dir, char* fileName)
+{
+    strcpy(buffer, dir);
+    strcat(buffer, "/");
+    return strcat(buffer, fileName);
+}
+
+class HotSpotFinder {
+protected:
+
+    double Delta, R2;  // main Cluster parameter (Delta=70m for R=100m )
+
+    double Xmin, Xmax, Ymin, Ymax;
+    int Xsize, Ysize;
+    vector<Waypoint>** clusterWaypoint;
+
+    int NumOfHotSpots;
+    vector<Location> Locs;
+
+public:
+    HotSpotFinder(char* boundFile, double delta, double r);
+    void LoadWaypointsFromDir(char* waypointDir);
+    bool isMergeable(vector<Waypoint>& wp1, vector<Waypoint>& wp2);
+    void mergeClusterToHotSpot( Cluster& hotSpot, vector<Waypoint>& clW );
+    void writeHotSpot(Cluster& cls, char* SpotFileName);
+    void printHotSpot(Cluster& HotSpot);
+    void makeAllSpots(char* hotSpotFilesDir);
+};
+
+HotSpotFinder::HotSpotFinder(char* boundFile, double delta=70, double r=100)
+{
+    Delta=delta; R2=r*r;
+
+    NumOfHotSpots=0;
+
+    ifstream* bf = new ifstream(boundFile);
+    if(bf==NULL) { cout << " No bound file\n"; exit(1); }
+    (*bf)>>Xmin>>Xmax>>Ymin>>Ymax;
+    bf->close();
+    cout<<"Read from bound file:\n";
+    cout<<"Xmin="<<Xmin<<"\t Xmax="<<Xmax<<"\n"<<"Ymin="<<Ymin<<"\t Ymax="<<Ymax<<endl<<endl;
+
+    Xmin=floor(Xmin/Delta)*Delta;  Xmax=ceil(Xmax/Delta)*Delta;  Xsize=(int)ceil((Xmax-Xmin)/Delta);
+    Ymin=floor(Ymin/Delta)*Delta;  Ymax=ceil(Ymax/Delta)*Delta;  Ysize=(int)ceil((Ymax-Ymin)/Delta);
+    cout<<"Adjusted to Delta grid:\n";
+    cout<<"Xmin="<<Xmin<<"\t Xmax="<<Xmax<<"\n"<<"Ymin="<<Ymin<<"\t Ymax="<<Ymax<<endl<<endl;
+    cout<<"Xsize="<<Xsize<<"\t Ysize="<<Ysize<<endl;
+
+    clusterWaypoint = new vector<Waypoint>*[Ysize];
+    for(int i=0; i<Ysize; i++) clusterWaypoint[i]=new vector<Waypoint>[Xsize];
+}
+
+
+void HotSpotFinder::LoadWaypointsFromDir(char* waypointDir)
+{
+    char waypointFileNamePattern[256];
+    buildFullName(waypointFileNamePattern, waypointDir, "*.wpt");
+    cout << "   traceFileNamePattern: " << waypointFileNamePattern << endl << endl;
+
+    Waypoint wp;
+    int i,k, counter=0;
+
+    WIN32_FIND_DATA f;
+    HANDLE h = FindFirstFile(waypointFileNamePattern, &f);
+    if(h != INVALID_HANDLE_VALUE)
+    {
+        do {
+            char inputFileName[256];
+            buildFullName(inputFileName, waypointDir, f.cFileName);
+
+            ifstream* file= new ifstream(inputFileName);
+            wp.traceName=new char[strlen(f.cFileName)+1];
+            strcpy(wp.traceName, f.cFileName);
+
+            while(!file->eof()) {
+                (*file)>>wp.X>>wp.Y>>wp.Tb>>wp.Te;
+                 i=(int)floor((wp.Y-Ymin)/Delta);
+                 k=(int)floor((wp.X-Xmin)/Delta);
+                 clusterWaypoint[i][k].push_back(wp);
+                 counter++;
+            }
+            cout<<endl<<counter<<" waypoints were loaded from file "<<f.cFileName<<endl;
+            file->close();
+        } while(FindNextFile(h, &f));
+    }
+    else
+    {
+        fprintf(stderr, "Directory or files not found\n");
+    }
+}
+
+
+bool HotSpotFinder::isMergeable(vector<Waypoint>& wp1, vector<Waypoint>& wp2)
+{
+    double dx, dy;
+    unsigned int i,j;
+
+    if( wp1.size()==0 || wp2.size()==0 ) return true;
+
+    for(i=0; i<wp1.size(); i++)
+    for(j=0; j<wp2.size(); j++){
+        dx=(wp1[i]).X-(wp2[j]).X;
+        dy=(wp1[i]).Y-(wp2[j]).Y;
+        if( dx*dx+dy*dy <= R2 ) return true;
+    }
+    return false;
+}
+
+void HotSpotFinder::mergeClusterToHotSpot( Cluster& hotSpot, vector<Waypoint>& clW )
+{
+    Waypoint h;
+
+    if(clW.size()==0) return;
+
+    h=clW[0];
+    if( (hotSpot.waypoint).size()==0) {
+        hotSpot.Xmin=hotSpot.Xmax=h.X;
+        hotSpot.Ymin=hotSpot.Ymax=h.Y;
+        hotSpot.sumTime=h.Te-h.Tb;
+       (hotSpot.waypoint).push_back(h);
+    } else {
+        if(h.X>hotSpot.Xmax) hotSpot.Xmax=h.X;
+        if(h.X<hotSpot.Xmin) hotSpot.Xmin=h.X;
+        if(h.Y>hotSpot.Ymax) hotSpot.Ymax=h.Y;
+        if(h.Y<hotSpot.Ymin) hotSpot.Ymin=h.Y;
+        hotSpot.sumTime+=h.Te-h.Tb;
+       (hotSpot.waypoint).push_back(h);
+    }
+
+    for(unsigned int i=1; i<clW.size(); i++) {
+        h=clW[i];
+        if(h.X>hotSpot.Xmax) hotSpot.Xmax=h.X;
+        if(h.X<hotSpot.Xmin) hotSpot.Xmin=h.X;
+        if(h.Y>hotSpot.Ymax) hotSpot.Ymax=h.Y;
+        if(h.Y<hotSpot.Ymin) hotSpot.Ymin=h.Y;
+        hotSpot.sumTime+=h.Te-h.Tb;
+       (hotSpot.waypoint).push_back(h);
+    }
+}
+
+
+void HotSpotFinder::makeAllSpots(char* hotSpotFilesDir)
+{
+    int i,k;
+    unsigned int j;
+    vector<int> iq, kq;
+    Cluster HotSpot;
+
+    WIN32_FIND_DATA f;
+    if (FindFirstFile(hotSpotFilesDir, &f) == INVALID_HANDLE_VALUE)
+    {
+        if (CreateDirectory(hotSpotFilesDir, NULL))
+            cout << "create output directory " << endl;
+        else
+            cout << "error create output directory" << endl;
+    }
+
+    for(i=0; i<Ysize; i++)
+    for(k=0; k<Xsize; k++)
+    if((clusterWaypoint[i][k]).size()>0) { iq.push_back(i); kq.push_back(k); };
+
+    while(iq.size()>0) {
+        (HotSpot.waypoint).clear();
+         mergeClusterToHotSpot( HotSpot,  clusterWaypoint[iq[0]][kq[0]]);
+         iq.erase(iq.begin());  kq.erase(kq.begin());
+         j=0;
+        while( j < iq.size()) {
+            if (isMergeable(HotSpot.waypoint, clusterWaypoint[iq[j]][kq[j]])) {
+                mergeClusterToHotSpot( HotSpot,  clusterWaypoint[iq[j]][kq[j]]);
+                iq.erase(iq.begin()+j);  kq.erase(kq.begin()+j);
+            } else j++;
+        }
+        NumOfHotSpots++; cout<<"Cluster number "<<NumOfHotSpots<<endl;
+        printHotSpot(HotSpot);
+
+        Location lc;
+        lc.Xmin=HotSpot.Xmin; lc.Xmax=HotSpot.Xmax; lc.Ymin=HotSpot.Ymin; lc.Ymax=HotSpot.Ymax;
+        lc.sumTime=HotSpot.sumTime; lc.wPoints=(HotSpot.waypoint).size();
+        Locs.push_back(lc);
+
+        char num[5];
+        char name[128];
+        char buffer[256];
+        itoa(NumOfHotSpots, num, 10);
+        strcpy(name, "hotSpot"); strcat(name, num); strcat(name, ".hts");
+        buildFullName(buffer, hotSpotFilesDir, name);
+        writeHotSpot(HotSpot, buffer);
+    }
+
+    ofstream lcfile("locations.loc");
+    for(unsigned int i=0; i<Locs.size(); i++) {
+        lcfile<<i+1<<"\t"<<(Locs[i]).sumTime<<"\t"<<"\t"<<(Locs[i]).wPoints<<"\t"<<"\t";
+        lcfile<<(Locs[i]).Xmin<<"\t"<<(Locs[i]).Xmax<<"\t";
+        lcfile<<(Locs[i]).Ymin<<"\t"<<(Locs[i]).Ymax<<endl;
+    }
+    lcfile.close();
+}
+
+void HotSpotFinder::writeHotSpot(Cluster& HotSpot, char* SpotFileName)
+{
+    ofstream file(SpotFileName);
+    file<<HotSpot.Xmin<<"\t"<<HotSpot.Xmax<<endl;
+    file<<HotSpot.Ymin<<"\t"<<HotSpot.Ymax<<endl;
+    file<<HotSpot.sumTime<<"\t"<<(HotSpot.waypoint).size()<<endl;
+    for(unsigned int i=0; i<(HotSpot.waypoint).size(); i++)
+        file<<(HotSpot.waypoint[i]).X <<"\t"<<(HotSpot.waypoint[i]).Y <<"\t"
+            <<(HotSpot.waypoint[i]).Tb<<"\t"<<(HotSpot.waypoint[i]).Te<<"\t"
+            <<(HotSpot.waypoint[i]).traceName<<endl;
+    file.close();
+}
+
+void HotSpotFinder::printHotSpot(Cluster& HotSpot)
+{
+    cout<<"Xmin="<<HotSpot.Xmin<<"\t Xmax="<<HotSpot.Xmax<<endl;
+    cout<<"Ymin="<<HotSpot.Ymin<<"\t Ymax="<<HotSpot.Ymax<<endl;
+    cout<<"Total time in cluster:"<< HotSpot.sumTime<<"sec \t Number of waypoints:"<<(HotSpot.waypoint).size()<<endl;
+    for(unsigned int i=0; i<(HotSpot.waypoint).size(); i++)
+        cout<<(HotSpot.waypoint[i]).X <<"\t"<<(HotSpot.waypoint[i]).Y <<"\t"
+            <<(HotSpot.waypoint[i]).Tb<<"\t"<<(HotSpot.waypoint[i]).Te<<"\t"
+            <<(HotSpot.waypoint[i]).traceName<<endl;
+}
+
+
+int main(int argc, char** argv)
+{
+    cout << "Hello world!" << endl;
+
+    char* wayPointFilesDir;    //full path name of directory
+    char* hotSpotFilesDir; //full path name of directory
+    switch(argc)
+    {
+    case 1 :
+        wayPointFilesDir="./waypointfiles";
+        hotSpotFilesDir ="./hotspotfiles";
+        break;
+    case 2 :
+        wayPointFilesDir=argv[1];
+        hotSpotFilesDir ="./hotspotfiles";
+        break;
+    case 3 :
+    default:
+        wayPointFilesDir=argv[1];
+        hotSpotFilesDir=argv[2];
+        break;
+    }
+
+    HotSpotFinder HSFinder("bounds.bnd");
+
+    HSFinder.LoadWaypointsFromDir(wayPointFilesDir);
+
+    HSFinder.makeAllSpots(hotSpotFilesDir);
+
+    return 0;
+}
---
+++ RootFinder/main.cpp
@@ -0,0 +1,326 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <cstdlib>
+#include <math.h>
+#include <queue>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+struct Waypoint{
+    double X,Y,Tb,Te;
+    char* traceName;
+};
+
+struct HotSpotVisit {
+    double Tb, Te;
+    char* HotSpot;
+};
+
+char* buildFullName(char* buffer, char* dir, char* fileName)
+{
+    strcpy(buffer, dir);
+    strcat(buffer, "/");
+    return strcat(buffer, fileName);
+}
+
+// -----------------------------------------------------------------------------------------------------
+// –еализаци€ двоичной кучи дл€ хранени€ путевых точек в пор€дке возрастани€ времени прихода
+
+class VisitMinHeap
+{
+private:
+    vector<HotSpotVisit> visit;
+
+    void BubbleDown(int index);
+    void BubbleUp(int index);
+
+public:
+
+    void Insert(HotSpotVisit& newValue);
+    bool GetMin(HotSpotVisit& Value);
+    void DeleteMin();
+};
+
+void VisitMinHeap::BubbleDown(int index)
+{
+    int length = visit.size();
+    int leftChildIndex = 2*index + 1;
+    int rightChildIndex = 2*index + 2;
+
+    if(leftChildIndex >= length)
+        return; //index is a leaf
+
+    int minIndex = index;
+
+    if( (visit[index]).Tb > (visit[leftChildIndex]).Tb)
+    {
+        minIndex = leftChildIndex;
+    }
+
+    if((rightChildIndex < length) && ( (visit[minIndex]).Tb > (visit[rightChildIndex]).Tb))
+    {
+        minIndex = rightChildIndex;
+    }
+
+    if(minIndex != index)
+    {
+        //need to swap
+        HotSpotVisit temp = visit[index];
+        visit[index] = visit[minIndex];
+        visit[minIndex] = temp;
+        BubbleDown(minIndex);
+    }
+}
+
+void VisitMinHeap::BubbleUp(int index)
+{
+    if(index == 0)
+        return;
+
+    int parentIndex = (index-1)/2;
+
+    if( (visit[parentIndex]).Tb > (visit[index]).Tb )
+    {
+        HotSpotVisit temp = visit[parentIndex];
+        visit[parentIndex] = visit[index];
+        visit[index] = temp;
+        BubbleUp(parentIndex);
+    }
+}
+
+void VisitMinHeap::Insert(HotSpotVisit& newValue)
+{
+    int length = visit.size();
+    visit.push_back(newValue);
+
+    BubbleUp(length);
+}
+
+bool VisitMinHeap::GetMin(HotSpotVisit& Value)
+{
+    if( visit.size() == 0) return false;
+
+    Value.Tb=(visit[0]).Tb;
+    Value.Te=(visit[0]).Te;
+    strcpy(Value.HotSpot, (visit[0]).HotSpot);
+    return true;
+}
+
+void VisitMinHeap::DeleteMin()
+{
+    int length = visit.size();
+
+    if(length == 0)
+    {
+        return;
+    }
+
+    visit[0] = visit[length-1];
+    visit.pop_back();
+
+    BubbleDown(0);
+}
+
+// -------------------------------------------------------------------------------------------------
+
+class RootFinder {
+protected:
+    vector<char*> UserNames;
+    VisitMinHeap* visitPoint;
+
+public:
+    RootFinder(char* wayPointFilesDir);
+    void loadHotSpotFiles(char* hotSpotFilesDir);
+    void writeAllRoots(char* rootFilesDir);
+};
+
+RootFinder::RootFinder(char* wayPointFilesDir)
+{
+    //  онструктор составл€ет спсок всех пользователей (имЄн файлов в директории путевых точек)
+    // и помещает его в вектор UserNames. ƒл€ каждого пользовател€ он заводит двоичную кучу
+    // дл€ хранени€ путевых точек в пор€дке возрастани€ времени прихода
+
+    char waypointFileNamePattern[256];
+    buildFullName(waypointFileNamePattern, wayPointFilesDir, "*.wpt");
+    cout << "   wayPointFileNamePattern: " << waypointFileNamePattern << endl << endl;
+
+    WIN32_FIND_DATA f;
+    if (FindFirstFile(wayPointFilesDir, &f) == INVALID_HANDLE_VALUE)
+        { cout << "no waypoint directory " << endl; exit(1); }
+
+    HANDLE h = FindFirstFile(waypointFileNamePattern, &f);
+    if(h != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            char UserName[256];
+            buildFullName(UserName, wayPointFilesDir, f.cFileName);
+            char* buffer=new char[256];
+            strcpy(buffer, f.cFileName);
+            UserNames.push_back(buffer);
+        }
+        while(FindNextFile(h, &f));
+    }
+    else
+    {
+        fprintf(stderr, "Directory or files not found\n");
+    }
+
+    for(unsigned int i=0; i<UserNames.size(); i++) cout<<UserNames[i]<<endl;
+    visitPoint = new VisitMinHeap[UserNames.size()];
+}
+
+void RootFinder::loadHotSpotFiles(char* hotSpotFilesDir)
+{
+    // »з всех файлов в директории локаций читает данные о путевых точках и распредел€ет их
+    // по двоичным кучам соответствующих пользователей c указанием локации каждой путевой точки
+
+    HotSpotVisit hsv;
+    double Xmin, Xmax, Ymin, Ymax, Tsum, X, Y;
+    int numWp;
+
+
+    char hotspotFileNamePattern[256];
+    buildFullName(hotspotFileNamePattern, hotSpotFilesDir, "*.hts");
+    cout << "   hotspotFileNamePattern: " << hotspotFileNamePattern << endl << endl;
+
+    WIN32_FIND_DATA f;
+    if (FindFirstFile(hotSpotFilesDir, &f) == INVALID_HANDLE_VALUE)
+        { cout << "no hotspot directory " << endl; exit(1); }
+
+    HANDLE h = FindFirstFile(hotspotFileNamePattern, &f);
+    if(h != INVALID_HANDLE_VALUE)
+    {
+        do {
+            char HotSpotFile[256];
+            buildFullName(HotSpotFile, hotSpotFilesDir, f.cFileName);
+            hsv.HotSpot=new char[strlen(f.cFileName)+1];
+            strcpy(hsv.HotSpot, f.cFileName);
+
+            char buffer[256];
+
+            ifstream file(HotSpotFile);
+            file>>Xmin>>Xmax>>Ymin>>Ymax>>Tsum>>numWp;
+            while(!file.eof()) {
+                for(int i=0; i<256; i++) buffer[i]='\0';
+                file>>X>>Y>>hsv.Tb>>hsv.Te>>buffer;
+                for(unsigned int i=0; i<UserNames.size();i++)
+                    if( strcmp(UserNames[i], buffer) == 0 ) { (visitPoint[i]).Insert(hsv); break; }
+            }
+            file.close(); cout<<hsv.HotSpot<<" loaded"<<endl;
+
+        } while(FindNextFile(h, &f));
+    }
+    else
+    {
+        fprintf(stderr, "Directory or files not found\n");
+    }
+}
+
+
+void RootFinder::writeAllRoots(char* rootFilesDir)
+{
+    // „итает двоичную кучу путевых точек каждого пользовател€ и записывает в файл в директории маршрутов
+    // пор€док следовани€ локаций в пути этого пользовател€ (со временеим и кол-вом путевых точек в каждой).
+    // “акже стрит гистограмму количества локаций в пут€х пользователей и записывает в файл length.hst
+    WIN32_FIND_DATA f;
+    if (FindFirstFile(rootFilesDir, &f) == INVALID_HANDLE_VALUE)
+    {
+        if (CreateDirectory(rootFilesDir, NULL))
+            cout << "create output directory " << endl;
+        else
+            cout << "error create output directory" << endl;
+    }
+
+    HotSpotVisit hsv, nexthsv;
+    hsv.HotSpot=new char[256];
+    nexthsv.HotSpot=new char[256];
+    double Tsum=0; int counter=0;
+    char buffer[256];
+
+    int rootLength[UserNames.size()];
+    long int rootT[UserNames.size()], rootP[UserNames.size()];
+    for(unsigned int i=0; i<UserNames.size(); i++) { rootLength[i]=0; rootT[i]=rootP[i]=0; }
+    int maxLength=0; long int rootTime, rootPoints;
+
+    for(unsigned int i=0; i<UserNames.size(); i++)
+    {
+        buildFullName(buffer, rootFilesDir, UserNames[i]);
+        strcat(buffer, ".rot");
+        ofstream file(buffer);
+        rootTime=0; rootPoints=0;
+        while ((visitPoint[i]).GetMin(hsv)) {
+            (visitPoint[i]).DeleteMin();
+            Tsum=hsv.Te-hsv.Tb; counter=1;
+            while( (visitPoint[i]).GetMin(nexthsv) && strcmp(hsv.HotSpot, nexthsv.HotSpot)==0) {
+                    (visitPoint[i]).DeleteMin();
+                    Tsum+=nexthsv.Te-nexthsv.Tb; counter++;
+            }
+            file<<hsv.HotSpot<<"\t"<<Tsum<<"\t"<<counter<<endl;
+            rootLength[i]++; rootTime+=Tsum; rootPoints+=counter;
+        }
+        file.close();
+        if(rootLength[i]>maxLength) maxLength=rootLength[i];
+        rootT[i]=rootTime; rootP[i]=rootPoints;
+    }
+    delete[] hsv.HotSpot;
+    delete[] nexthsv.HotSpot;
+
+    int lengthHistogram[maxLength+1];
+    long int rT[maxLength+1], rP[maxLength+1];
+    for(int i=0; i<=maxLength; i++) { lengthHistogram[i]=0; rT[i]=rP[i]=0; }
+    for(unsigned int i=0; i<UserNames.size(); i++) {
+        lengthHistogram[rootLength[i]]++;
+        rT[rootLength[i]]+=rootT[i]; rP[rootLength[i]]+=rootP[i];
+    }
+    ofstream lhst("length.hst");
+    for(int i=0; i<=maxLength; i++) lhst<<i<<"\t"<<lengthHistogram[i]<<"\t"<<rT[i]<<"\t"<<rP[i]<<endl;
+    lhst.close();
+}
+
+
+int main(int argc, char** argv)
+{
+    cout << "Hello world!" << endl;
+
+    char* wayPointFilesDir;    //full path name of way point files directory
+    char* hotspotFilesDir;     //full path name of hot spot files directory
+    char* rootFilesDir;        //full path name of root files directory
+    switch(argc)
+    {
+    case 1 :
+        wayPointFilesDir="./waypointfiles";
+        hotspotFilesDir="./hotspotfiles";
+        rootFilesDir="./rootfiles";
+        break;
+    case 2 :
+        wayPointFilesDir=argv[1];
+        hotspotFilesDir="./hotspotfiles";
+        rootFilesDir="./rootfiles";
+        break;
+    case 3 :
+        wayPointFilesDir=argv[1];
+        hotspotFilesDir=argv[2];
+        rootFilesDir="./rootfiles";
+        break;
+    case 4:
+    default:
+        wayPointFilesDir=argv[1];
+        hotspotFilesDir=argv[2];
+        rootFilesDir=argv[3];
+        break;
+    }
+
+    RootFinder h(wayPointFilesDir);
+    h.loadHotSpotFiles(hotspotFilesDir);
+    h.writeAllRoots(rootFilesDir);
+
+    return 0;
+}
