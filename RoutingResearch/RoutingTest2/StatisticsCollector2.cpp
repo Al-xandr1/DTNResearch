21ee2f5371333a1fd90844760daa6f4323768a12
al-xandr1 <al-xandr1@yandex.ru>
2017-01-14 15:04:30 +0300

    Made collection of common route histogram

--- RoutingResearch/RoutingTest2/StatisticsCollector2.cpp
+++ RoutingResearch/RoutingTest2/StatisticsCollector2.cpp
@@ -22,6 +22,8 @@ void StatisticsCollector2::initialize()
     ictPDF = new cDoubleHistogram("ICT-HISTOGRAM", 300);
     ictPDF->setRangeAutoUpper(0.0, 300, 1.3);
 
+    commonRoutesDurationPDF = NULL;
+
     routesDurationPDFbyNode = new vector<cDoubleHistogram*>();
 
     cout << "StatisticsCollector2: initialized" << endl;
@@ -108,6 +110,9 @@ void StatisticsCollector2::processRouteHistory() {
     ASSERT(routesDurationPDFbyNode->size() == 0);
     double maxDayDuration = atof(routeHistoryDoc->getAttribute("maxDayDuration"));
 
+    commonRoutesDurationPDF = new cDoubleHistogram("COMMON-ROUTE-DURATION-HISTOGRAM", 10);
+    commonRoutesDurationPDF->setRange(0.0, maxDayDuration + 1);
+
     cXMLElementList nodes = routeHistoryDoc->getChildren();
     for(vector<cXMLElement*>::iterator nodePT = nodes.begin(); nodePT != nodes.end(); nodePT++) {
          cXMLElement* node = (*nodePT);
@@ -135,48 +140,65 @@ void StatisticsCollector2::processRouteHistory() {
              double duration = dayEnd - dayStart;
              ASSERT(duration > 0);
              routeDurationHist->collect(duration);
+             commonRoutesDurationPDF->collect(duration);
          }
     }
 }
 
 //todo сделать запись ВСЕХ гистограмм через этот метод
-void StatisticsCollector2::write(cDoubleHistogram* hist, ofstream* out) {
-    (*out) << "            <"<<hist->getName()<<"> "<< endl;
-    (*out) << "                <COLLECTED> " << hist->getCount() << " </COLLECTED>" << endl;
-    (*out) << "                <MIN> " << hist->getMin() << " </MIN>" << endl;
-    (*out) << "                <MEAN> " << hist->getMean() << " </MEAN>" << endl;
-    (*out) << "                <MAX> " << hist->getMax() << " </MAX>" << endl;
-    (*out) << "                <VARIANCE> " << hist->getVariance() << " </VARIANCE>" << endl;
-    (*out) << "                <OVERFLOW-CELL> " << hist->getOverflowCell() << " </OVERFLOW-CELL>" << endl;
-    (*out) << "                <UNDERFLOW-CELL> " << hist->getUnderflowCell() << " </UNDERFLOW-CELL>" << endl;
+void StatisticsCollector2::write(cDoubleHistogram* hist, ofstream* out, int level) {
+    ASSERT(hist);
+    char* prefix = new char[256];
+    for (int i=0; i<level; i++) {
+        if (i == 0) {
+            prefix = strcpy(prefix, "    ");
+        } else {
+            prefix = strcat(prefix, "    ");
+        }
+    }
 
-    (*out) << "                <CELLS> " << hist->getNumCells() << " </CELLS>"<< endl;
-    (*out) << "                <CELL-WIDTH> " << hist->getCellSize() << " </CELL-WIDTH>"<< endl;
-    (*out) << "                <LEFT-BOUND> " << hist->getCellInfo(0).lower << " </LEFT-BOUND>" << endl;
-    (*out) << "                <RIGHT-BOUND> " << hist->getCellInfo(hist->getNumCells()-1).upper << " </RIGHT-BOUND>"<< endl;
+    (*out) << prefix << "<"<<hist->getName()<<"> "<< endl;
+    (*out) << prefix << "    <COLLECTED> " << hist->getCount() << " </COLLECTED>" << endl;
+    (*out) << prefix << "    <MIN> " << hist->getMin() << " </MIN>" << endl;
+    (*out) << prefix << "    <MEAN> " << hist->getMean() << " </MEAN>" << endl;
+    (*out) << prefix << "    <MAX> " << hist->getMax() << " </MAX>" << endl;
+    (*out) << prefix << "    <VARIANCE> " << hist->getVariance() << " </VARIANCE>" << endl;
+    (*out) << prefix << "    <OVERFLOW-CELL> " << hist->getOverflowCell() << " </OVERFLOW-CELL>" << endl;
+    (*out) << prefix << "    <UNDERFLOW-CELL> " << hist->getUnderflowCell() << " </UNDERFLOW-CELL>" << endl;
 
-    (*out) << "                <PDF-VALS> " << endl;
+    (*out) << prefix << "    <CELLS> " << hist->getNumCells() << " </CELLS>"<< endl;
+    (*out) << prefix << "    <CELL-WIDTH> " << hist->getCellSize() << " </CELL-WIDTH>"<< endl;
+    (*out) << prefix << "    <LEFT-BOUND> " << hist->getCellInfo(0).lower << " </LEFT-BOUND>" << endl;
+    (*out) << prefix << "    <RIGHT-BOUND> " << hist->getCellInfo(hist->getNumCells()-1).upper << " </RIGHT-BOUND>"<< endl;
+
+    (*out) << prefix << "    <PDF-VALS> " << endl;
     for (int i = 0; i < hist->getNumCells(); i++) (*out) << hist->getCellPDF(i) << "  ";
-    (*out) << endl << "                </PDF-VALS> " << endl;
-    (*out) << "                <CDF-VALS> " << endl;
+    (*out) << endl << prefix << "    </PDF-VALS> " << endl;
+    (*out) << prefix << "    <CDF-VALS> " << endl;
     for (int i = 0; i < hist->getNumCells(); i++) {
         double val = 0;
         for (int j = 0; j <= i; j++) val += hist->getCellPDF(j);
         (*out) << val << "  ";
     }
-    (*out) << endl << "                </CDF-VALS> " << endl;
-    (*out) << "                <CCDF-VALS> " << endl;
+    (*out) << endl << prefix << "    </CDF-VALS> " << endl;
+    (*out) << prefix << "    <CCDF-VALS> " << endl;
     for (int i = 0; i < hist->getNumCells(); i++) {
         double val = 0;
         for (int j = i+1; j < hist->getNumCells(); j++) val += hist->getCellPDF(j);
         (*out) << val << "  ";
     }
-    (*out) << endl << "                </CCDF-VALS> " << endl;
-    (*out) << "            </"<<hist->getName()<<"> " << endl;
+    (*out) << endl << prefix << "    </CCDF-VALS> " << endl;
+    (*out) << prefix << "</"<<hist->getName()<<"> " << endl;
+
+    delete[] prefix;
 }
 
 
 void StatisticsCollector2::finish() {
+    ASSERT(lifeTimePDF);
+    ASSERT(ictPDF);
+    ASSERT(commonRoutesDurationPDF);
+
     if (!lifeTimePDF->isTransformed()) lifeTimePDF->transform();
     if (!ictPDF->isTransformed()) ictPDF->transform();
 
@@ -254,10 +276,15 @@ void StatisticsCollector2::finish() {
     out << endl << "        </CCDF-VALS> " << endl;
     out << "    </ICT-HISTOGRAM> " << endl << endl;
 
+
+    write(commonRoutesDurationPDF, &out, 1);
+    out << endl;
+
+
     out << "    <NODES-INFO> " << endl;
     for (unsigned int nodeId=0; nodeId<routesDurationPDFbyNode->size(); nodeId++) {
         out << "        <NODE nodeId=\""<<nodeId<<"\"> " << endl;
-        write((*routesDurationPDFbyNode)[nodeId], &out);
+        write((*routesDurationPDFbyNode)[nodeId], &out, 3);
         out << "        </NODE> " << endl;
     }
     out << "    </NODES-INFO> " << endl;
