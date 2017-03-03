dbf6786ed5b629efbe37037ba697cee20480b364
Al-xandr1 <Al-xandr1@users.noreply.github.com>
2017-03-03 14:27:08 +0400

    Found the way for solution 1

--- Utils/dartmouthCencemeDatasetReader/src/Main.java
+++ Utils/dartmouthCencemeDatasetReader/src/Main.java
@@ -13,26 +13,25 @@ public class Main {
     static private FileReader inputFile;
     static private FileReader outputFile;
 
+    public static final Pattern PATTERN = Pattern.compile("(?<TIMESTAMP>\\d*) DATA \\((?<NUMBER>\\d){0,3}\\) - (?<TAG>GPS|ACT|GPS-Skipped|ACC): (?<DATA>.*)");
+
+    // blocks of a line
+    public static final String TIMESTAMPE = "TIMESTAMP";
+    public static final String NUMBER = "NUMBER";
+    public static final String TAG = "TAG";
+    public static final String DATA = "DATA";
+
+    // tag's values
+    public static final String ACT = "ACT"; // данные о текущем состоянии
+    public static final String GPS = "GPS"; // gps данные
+    public static final String GPS_SKIPPED = "GPS-Skipped"; // GPS данные отсутствуют (пользователь бездействует более 15 минут)
+    public static final String ACC = "ACC"; // данные акселерометра
+
     public static final String TEST_GPS_LINE = "1216830985473 DATA (0) - GPS: 168.7,4342.38016,7217.17455,4.9,3.6*168.3,4342.38246,7217.16246,5.0,4.3*168.6,4342.3832,7217.16708,5.0,0.0*168.8,4342.38357,7217.17294,5.0,0.0*169.1,4342.38409,7217.17614,3.5,6.4*172.4,4342.38646,7217.17606,4.4,4.8*";
     public static final String TEST_ACT_LINE = "1216830974754 DATA (0) - ACT: 1216830959582,1216830973695,5";
 
     public static void main(String[] args) {
-//        processFile();
-        Pattern pattern = Pattern.compile("(?<TIMESTAMP>\\d*) DATA \\((?<NUMBER>\\d){0,3}\\) - (?<TAG>GPS|ACT|GPS-Skipped): (?<DATA>.*)");
-        Matcher matcher = pattern.matcher(TEST_GPS_LINE);
-
-        System.out.println(matcher.find());
-        System.out.println(matcher.group());
-        System.out.println(matcher.groupCount());
-        System.out.println();
-        for (int i = 0; i < matcher.groupCount(); i++) {
-            System.out.println(matcher.group(i));
-        }
-        System.out.println("\nby name\n");
-        System.out.println(matcher.group("TIMESTAMP"));
-        System.out.println(matcher.group("NUMBER"));
-        System.out.println(matcher.group("TAG"));
-        System.out.println(matcher.group("DATA"));
+        processFile();
     }
 
     private static void processFile() {
@@ -60,8 +59,43 @@ public class Main {
 
     private static void processLine(BufferedWriter bufferedWriter, String line) {
         //System.out.println(line);
-        Pattern pattern = Pattern.compile("");
+        Matcher matcher = PATTERN.matcher(line);
 
+        if (matcher.find()) {
+            final String timestamp = matcher.group("TIMESTAMP");
+//            System.out.println(timestamp);
+            final String number = matcher.group("NUMBER");
+//            System.out.println(number);
+            final String tag = matcher.group("TAG");
+//            System.out.println(tag);
+            final String data = matcher.group("DATA");
+//            System.out.println(data);
+
+            switch (tag) {
+                case GPS: {
+                    System.out.println(line);
+                    //todo определить временные точки для каждой позиции
+                    //todo записать все GPS данные в output файл
+                    break;
+                }
+                case ACC: {
+                    break;
+                }
+                case GPS_SKIPPED: {
+                    //todo записать в файл последнюю актуальную точку
+                    System.out.println(line);
+                    break;
+                }
+                case ACT: {
+//                    System.out.println(line);
+                    break;
+                }
+            }
+
+
+        } else {
+            System.out.println(line);
+        }
 
     }
 }
